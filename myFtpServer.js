ed07c5a70d4b47cd7386d5adc7e2656fa132a066
Yves kouayip <kamwayves37@gmail.com>
2020-11-22 16:13:34 +0100

    defined all available commands

---
+++ myFtpServer.js
@@ -0,0 +1,65 @@
+//? Defined <port>
+
+const net = require("net");
+
+//? Define all available commands
+const commands = {
+  USER: {
+    name: "USER",
+    requireParam: true,
+    desc: "<username>: check if the user exist",
+  },
+  PASS: {
+    name: "PASS",
+    requireParam: true,
+    desc: "<password>: authenticate the user with a password",
+  },
+  LIST: {
+    name: "LIST",
+    requireParam: false,
+    desc: "list the current directory of the server",
+  },
+  CWD: {
+    name: "CWD",
+    requireParam: true,
+    desc: "<directory>: change the current directory of the server",
+  },
+  RETR: {
+    name: "RETR",
+    requireParam: true,
+    desc:
+      "<filename>: transfer a copy of the file FILE from the server to the client",
+  },
+  STOR: {
+    name: "STOR",
+    requireParam: true,
+    desc:
+      "<filename>: transfer a copy of the file FILE from the client to the server",
+  },
+  PWD: {
+    name: "PWD",
+    requireParam: false,
+    desc: "display the name of the current directory of the server",
+  },
+  HELP: {
+    name: "HELP",
+    requireParam: false,
+    desc: "send helpful information to the client",
+  },
+  QUIT: {
+    name: "QUIT",
+    requireParam: false,
+    desc: "close the connection and stop the program",
+    action: () => {},
+  },
+};
+
+//? Display all available commands
+const showCommands = () => {
+  let message = "";
+  Object.values(commands).forEach((command) => {
+    message += `${command.name}: ${command.desc}\n`;
+  });
+  return message;
+};
+console.log(showCommands());
